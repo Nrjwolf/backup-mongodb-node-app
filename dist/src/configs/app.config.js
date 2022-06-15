16e743ee728aaca7eb260837163f9fb1a2a002e9
nrjwolf <nrjwolf@gmail.com>
2022-06-15 22:54:11 +0800

    remove js

--- dist/src/configs/app.config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    DUMP_PROCESS_INTERVAL: 6
-};
--- dist/src/configs/env.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var _a, _b, _c, _d;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    TELEGRAM_BOT_TOKEN: (_a = process.env.TELEGRAM_BOT_TOKEN) !== null && _a !== void 0 ? _a : '',
-    TELEGRAM_CHAT_TO_LOG: (_b = process.env.TELEGRAM_CHAT_TO_LOG) !== null && _b !== void 0 ? _b : '',
-    ADMIN_TELEGRAM_ID: (_c = process.env.ADMIN_TELEGRAM_ID) !== null && _c !== void 0 ? _c : '',
-    DUMP_PROCESS_INTERVAL: (_d = process.env.DUMP_PROCESS_INTERVAL) !== null && _d !== void 0 ? _d : '',
-};
